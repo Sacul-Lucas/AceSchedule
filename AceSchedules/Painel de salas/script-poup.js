9659770e6ee3b9e707747762dc48d66040950d35
vicctorxavier <146958488+vicctorxavier@users.noreply.github.com>
2023-10-19 23:09:24 -0300

    Add files via upload

---
+++ AceSchedules/Painel de salas/script-poup.js
@@ -0,0 +1,59 @@
+const btnSala1 = document.getElementById(`btnSala1`);
+const fechar = document.getElementById(`close`);
+const modal1 = document.getElementById(`PoupUp1`);
+
+btnSala1.onclick = function () {
+    modal1.showModal()
+}
+ 
+fechar.onclick = function () {
+    modal1.close()
+}
+
+const btnSala2 = document.getElementById(`btnSala2`);
+const fechar2 = document.getElementById(`close2`);
+const modal2 = document.getElementById(`PoupUp2`);
+
+btnSala2.onclick = function () {
+    modal2.showModal()
+}
+ 
+fechar2.onclick = function () {
+    modal2.close()
+}
+
+const btnSala3 = document.getElementById(`btnSala3`);
+const fechar3 = document.getElementById(`close3`);
+const modal3 = document.getElementById(`PoupUp3`);
+
+btnSala3.onclick = function () {
+    modal3.showModal()
+}
+ 
+fechar3.onclick = function () {
+    modal3.close()
+}
+
+const btnSala4 = document.getElementById(`btnSala4`);
+const fechar4 = document.getElementById(`close4`);
+const modal4 = document.getElementById(`PoupUp4`);
+
+btnSala4.onclick = function () {
+    modal4.showModal()
+}
+ 
+fechar4.onclick = function () {
+    modal4.close()
+}
+
+const btnSala5 = document.getElementById(`btnSala5`);
+const fechar5 = document.getElementById(`close5`);
+const modal5 = document.getElementById(`PoupUp5`);
+
+btnSala5.onclick = function () {
+    modal5.showModal()
+}
+ 
+fechar5.onclick = function () {
+    modal5.close()
+}
